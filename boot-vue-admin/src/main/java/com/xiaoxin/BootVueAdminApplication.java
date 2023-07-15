bb580e5c58fd8bd1eeb762b1adb3c7ad5e1aec3a
xiaoxin179 <97381599+xiaoxin179@users.noreply.github.com>
2023-07-15 21:57:06 +0800

    第一次提交所有的代码

---
+++ boot-vue-admin/src/main/java/com/xiaoxin/BootVueAdminApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootVueAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootVueAdminApplication.class, args);
+    }
+
+}
---
+++ boot-vue-admin/src/test/java/com/xiaoxin/BootVueAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoxin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootVueAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
